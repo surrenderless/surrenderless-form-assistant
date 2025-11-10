7f1e11d00d7c8cea7ed93f7b6004378638481e19
Yih Sohn <surrenderlessbrands@gmail.com>
2025-11-09 18:01:37 -0800

    force rebuild: upstash rate limiter

--- src/utils/rateLimiter.ts
+++ src/utils/rateLimiter.ts
@@ -1,6 +1,10 @@
-import Redis from "ioredis";
+// force rebuild
+import { Redis } from "@upstash/redis";
 
-const redis = new Redis(process.env.UPSTASH_REDIS_URL!);
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL!,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN!,
+});
 
 export async function rateLimit(userId: string) {
   const key = `rate:${userId}`;
