6d4cb6bff4f5bd26ee89266fd104f1a0c9ec1eb3
Yih Sohn <surrenderlessbrands@gmail.com>
2025-11-09 18:09:32 -0800

    add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div style={{ textAlign: "center", padding: "50px" }}>
+      <h1>404 - Page Not Found</h1>
+      <p>Sorry, the page you’re looking for doesn’t exist.</p>
+    </div>
+  );
+}
